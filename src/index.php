4c6c2265037b06104a5ffa11c0c7491fd282d5a5
AnnKodynska <kotka219@gmail.com>
2019-12-09 22:03:46 +0200

    added to Docker hub

--- src/index.php
+++ src/index.php
@@ -1,14 +1,14 @@
 <?php
-$conn = new mysqli("database", "root", ".sweetpwd.", "my_db");
+$conn = new mysqli("127.0.0.1", "root", "kotka", "geonames");
 // Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT name FROM countryInfo";
-//$result = $conn->query($sql);
-$result = mysqli_query($conn, $sql);
-print_r($result);
+$result = $conn->query($sql);
+//$result = mysqli_query($conn, $sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
